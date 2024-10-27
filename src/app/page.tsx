091476f5bf8a59453b5ad3342cb406fef7b1307a
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-10-27 22:19:17 +0500

    completed edit function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,52 +3,76 @@ import { allTodoType } from "@/types/types";
 import { useState } from "react";
 
 export default function Home() {
-  const [todo, setTodo] = useState<string>("")
-  const [allTodos, setAllTodos] = useState<allTodoType[]>([])
+  const [todo, setTodo] = useState<string>("");
+  const [allTodos, setAllTodos] = useState<allTodoType[]>([]);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [editIndex, setEditIndex] = useState<number | null>(null);
+
   function handleSubmit() {
-    setTodo(todo)
-    saveTodo()
+    if (isEditing && editIndex !== null) {
+      updateTodo();
+    } else {
+      saveTodo();
+    }
   }
+
   function saveTodo() {
-    const copyTodos = [...allTodos]
-    copyTodos.push({ name: todo })
-    setAllTodos(copyTodos)
-    setTodo("")
+    const copyTodos = [...allTodos];
+    copyTodos.push({ name: todo });
+    setAllTodos(copyTodos);
+    setTodo("");
   }
+
   function deleteTodo(index: number) {
-    const copyTodos = [...allTodos]
-    copyTodos.splice(index, 1)
-    setAllTodos(copyTodos)
+    const copyTodos = [...allTodos];
+    copyTodos.splice(index, 1);
+    setAllTodos(copyTodos);
   }
+
   function editTodo(index: number) {
-    const copyTodos = [...allTodos]
-    copyTodos.splice(index, 1)
-    setAllTodos(copyTodos)
+    setIsEditing(true);
+    setEditIndex(index);
+    setTodo(allTodos[index].name); 
   }
+
+  function updateTodo() {
+    if (editIndex !== null) {
+      const copyTodos = [...allTodos];
+      copyTodos[editIndex] = { name: todo };
+      setAllTodos(copyTodos);
+      setIsEditing(false);
+      setEditIndex(null);
+      setTodo("");
+    }
+  }
+
   return (
     <>
       <h1>Todo</h1>
-      <input type="text"
+      <input
+        type="text"
         onChange={(e) => setTodo(e.target.value)}
-        value={todo} />
+        value={todo}
+      />
       <button onClick={handleSubmit}>
-        ADD
+        {isEditing ? "Update" : "Add"}
       </button>
       <br />
       <br />
-      <h3>All todos : </h3>
+      <h3>All todos:</h3>
       <br />
       <p>
-        {allTodos.map(({ name }, index) => {
-          return <b key={index}>
-            Todo : {name}
+        {allTodos.map(({ name }, index) => (
+          <b key={index}>
+            Todo: {name}
             <br />
-            <button onClick={() => deleteTodo(index)}>delete</button>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
             <br />
-            <button onClick={() => editTodo(index)}>edit</button>
-            <br /><br />
+            <button onClick={() => editTodo(index)}>Edit</button>
+            <br />
+            <br />
           </b>
-        })}
+        ))}
       </p>
     </>
   );
